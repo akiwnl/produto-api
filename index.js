e26eb72d683ef5ae4cb16db0ee523acc5f886001
akiwnl <carvalhokaike69@gmail.com>
2024-10-22 19:40:51 -0300

    create server using express

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const productRoutes = require("./routes/productRoutes");
+
 const PORT = 3000;
 const app = express();
+const db = require("./db");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
+app.use("/", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
