c9eb9442b41245569af7ffa26620a507d16bc9ac
akiwnl <carvalhokaike69@gmail.com>
2024-11-06 19:41:48 -0300

    add console.log

--- src/db.js
+++ src/db.js
@@ -1,27 +1,26 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 const fs = require("fs");
-const path = require('path')
+const path = require("path");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-
 const runScript = async (filePath) => {
   const script = fs.readFileSync(path.join(__dirname, filePath)).toString();
 
   try {
-      await pool.query(script);
+    await pool.query(script);
+    console.log(`Script ${filePath} rodado com sucesso!`);
   } catch (error) {
-      console.error(`Erro ao rodar script ${filePath}:`, error.message);
-  } 
+    console.error(`Erro ao rodar script ${filePath}:`, error.message);
+  }
 };
 
 const runAllScripts = async () => {
-  // Execute os scripts na ordem correta
-  await runScript('./createTable.sql');
-  await runScript('./seed.sql');
+  await runScript("./createTable.sql");
+  await runScript("./seed.sql");
 };
 
 runAllScripts();
