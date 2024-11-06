5b881ba23d3f36ff3fc6424ce5060fed114e407c
akiwnl <carvalhokaike69@gmail.com>
2024-11-06 19:41:41 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const productRoutes = require("./routes/productRoutes");
-
+const cors = require("cors");
 const PORT = 3000;
 const app = express();
 const db = require("./db");
 
+app.use(cors());
 app.use(express.json());
 app.use("/", productRoutes);
 
