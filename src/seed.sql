bf1e9cc28beb24563a6d95b371cf0cf64a47ed6f
Sara Guaiume <randesungasabonete@gmail.com>
2024-11-06 22:31:36 -0300

    change names

--- src/seed.sql
+++ src/seed.sql
@@ -1,6 +1,6 @@
 INSERT INTO products (description, price, quantity) VALUES
-('Garrafa de Agua', 1.99, 100),
-('Pao de Forma', 5.99, 50),
-('Chocolate', 4.99, 30),
-('Bombom', 1.99, 130),
-('Suco de Laranja', 10.99, 25);
+('Coca Cola', 1.99, 100),
+('Tuiubaina', 5.99, 50),
+('Fanta uva', 4.99, 30),
+('Pepsi', 1.99, 130),
+('Pepsi Twist', 10.99, 25);
